6f577605b031c1ea4905d8be7ee530815c500231
jcarrascal <juandavidcarrascalberdugo@gmail.com>
2024-09-19 11:28:40 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miLogo from "./assets/img/dd7e3c3911e171e2c5d0279b24b63d62.png"
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <img src={miLogo} alt= "The GOAT !" />
+      <h1> Chamber  </h1>
+      <p>Head Hunter</p>
+     </div>
+    </>
+  )
+}
+
+export default App
